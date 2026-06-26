6ae8a19a39c326bab1aab9c788d2f3351e2f6abe
vinith <vinithdc@gmail.com>
2026-06-26 21:48:10 +0530

    feat: add random quote generator

---
+++ rand_Chocice.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Success comes from consistency.",
+    "Never stop learning.",
+    "Dream big and work hard.",
+    "Believe in yourself.",
+    "Code. Learn. Repeat."
+]
+
+print(random.choice(quotes))
